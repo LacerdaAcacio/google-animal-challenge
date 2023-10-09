27cd085c2dc1f5e11e4fb932256b2cd0d39e6722
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-10-08 21:02:06 -0300

    feat: :truck: [ROUTE]routes
    *Created array of routes to use as react router map;

---
+++ src/routes/routes.ts
@@ -0,0 +1,15 @@
+import HomePage from "../pages/HomePage";
+import SearchPage from "../pages/SearchPage";
+
+const routes = [
+  {
+    path: "/",
+    Component: HomePage,
+  },
+  {
+    path: "/search",
+    Component: SearchPage,
+  },
+];
+
+export default routes;
