283164cb88da497fb0c53275a5823f3a7c0f0ed5
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-10-09 17:14:00 -0300

    style: :lipstick: [STYLE]styles
    *Added UserAvatar and StyledAppContainer; *Adjusted HomeContainer and SearchPageContainer.

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,17 +1,34 @@
 import styled from "styled-components";
 
 export const HomeContainer = styled.div`
-  height: 100%;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap: 15px;
 `;
 
 export const SearchPageContainer = styled.div`
-  display: flex;
+  /* display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+  height: 100%; */
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const UserAvatar = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+`;
+
+export const StyledAppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
   height: 100%;
 `;
