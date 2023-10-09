5c740da565692ab02b7b918529e23b76d0619559
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-10-09 18:19:22 -0300

    feat: :art: [SRC]App
    *Added StyledAppContainer; *Adjusted import of LABELS.

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,44 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
 import { faker } from "@faker-js/faker";
 import Footer from "./components/Footer";
-import LABELS from "./constants";
+import { LABELS } from "./constants";
 import routes from "./routes/routes";
 import { RouteProps } from "./types/PropsData";
 import Header from "./components/Header";
 import useHiddenSearch from "./hooks/contexts/useHiddenSearch";
+import { StyledAppContainer } from "./styles/styles";
 
 function App() {
   const { hiddenSearch } = useHiddenSearch();
   const avatar = faker.image.avatar();
   return (
     <Router>
-      <Header
-        srcAvatar={avatar}
-        altAvatar={LABELS.ALT_AVATAR}
-        type="text"
-        placeholder={`${LABELS.SEARCH}...`}
-        hiddenSearch={hiddenSearch}
-      />
-      <Routes>
-        {routes.map((route: RouteProps) => {
-          return (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={React.createElement(route.Component)}
-            />
-          );
-        })}
-      </Routes>
-      <Footer
-        key="footer"
-        label={LABELS.FOOTER}
-        version={LABELS.VERSION}
-        showCopyRight
-      />
+      <StyledAppContainer>
+        <Header
+          srcAvatar={avatar}
+          altAvatar={LABELS.ALT_AVATAR}
+          type="text"
+          placeholder={`${LABELS.SEARCH}...`}
+          hiddenSearch={hiddenSearch}
+        />
+        <Routes>
+          {routes.map((route: RouteProps) => {
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={React.createElement(route.Component)}
+              />
+            );
+          })}
+        </Routes>
+        <Footer
+          key="footer"
+          label={LABELS.FOOTER}
+          version={LABELS.VERSION}
+          showCopyRight
+        />
+      </StyledAppContainer>
     </Router>
   );
 }
