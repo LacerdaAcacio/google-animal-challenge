b2728a44ee15b88b4ff22bd8bb1ccdac5eb60cfa
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-10-08 21:50:18 -0300

    feat: :art: [CONSTANTS]constants
    *Created LABELS: VERSION, SEARCH, ALT_AVATAR; *Updated the value of FOOTER; *Exported default LABELS (there was only one object inside the component).

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,17 +1,10 @@
 import { LabelsData } from "../types/ConstantsData";
 
-export const MILLISECONDS_IN_A_DAY: number = 1000 * 60 * 60 * 24;
-export const API_KEY: string = "RVZG0GHEV2KORLNA";
-
-export const EXCHANGE_URL: string =
-  "https://api-brl-exchange.actionlabs.com.br/api/1.0/open/";
-
-export const LABELS: LabelsData = {
-  INPUT: "Enter the currency code",
-  BUTTON: "Exchange Result",
-  FOOTER: "Copyright 2022 - Action Labs",
-  CURRENT_TITLE: "Exchange Rate now",
-  MAIN_TITLE: "BRL Exchange Rate",
-  INVALID_VALUE: "Insira um código de moeda válido como JPY, USD, EUR, etc.",
-  DAILY_TITLE: "Last 30 days",
+const LABELS: LabelsData = {
+  FOOTER: "Acacio 2021",
+  VERSION: "0.1.0",
+  SEARCH: "Search",
+  ALT_AVATAR: "Avatar",
 };
+
+export default LABELS;
