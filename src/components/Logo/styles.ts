697bd36188fc8819976c27c8d22a583910615c85
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-10-08 21:56:27 -0300

    style: :lipstick: [STYLE]Logo
    *Created StyledLogo;

---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { ReactComponent as LogoComponent } from "../../assets/images/svg/logo.svg";
+
+const StyledLogo = styled(LogoComponent)`
+  width: 100%;
+  max-width: 320px;
+  margin: 0 auto;
+  display: flex;
+`;
+
+export default StyledLogo;
