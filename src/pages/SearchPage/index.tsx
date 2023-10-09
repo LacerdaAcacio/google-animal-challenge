0930ec2d737bebd7d687a7f3169529cd5d6a830f
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-10-08 21:44:25 -0300

    feat: :art: [PAGE]SearchPage
    *Created SearchPage;

---
+++ src/pages/SearchPage/index.tsx
@@ -0,0 +1,30 @@
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { SearchPageContainer } from "../../styles/styles";
+import SearchResult from "../../components/Search/SearchResult";
+import { AnimalsInfoData } from "../../types/AnimalsInfoData";
+import useHiddenSearch from "../../hooks/contexts/useHiddenSearch";
+// import { useHiddenSearch } from "../../contexts/HiddenSearchContext";
+
+function SearchPage() {
+  const location = useLocation();
+  const { setHiddenSearch } = useHiddenSearch();
+  const filteredAnimals = location?.state?.animalData;
+  const hasFilteredAnimals = Boolean(filteredAnimals?.length > 0);
+  // console.log(filteredAnimals);
+
+  useEffect(() => {
+    setHiddenSearch(false);
+  }, [setHiddenSearch]);
+
+  return (
+    <SearchPageContainer>
+      {hasFilteredAnimals &&
+        filteredAnimals.map((filteredAnimal: AnimalsInfoData) => {
+          return <SearchResult key={filteredAnimal.id} {...filteredAnimal} />;
+        })}
+    </SearchPageContainer>
+  );
+}
+
+export default SearchPage;
