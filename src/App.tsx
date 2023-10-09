883ddcfbd2333ae8e54ef20393a3051037ae4529
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-10-08 22:12:42 -0300

    feat: :art: [SRC]App.tsx
    *Added Header and Footer (both will be displayed on all pages); *Added Routes performing a map on the routes set in routes; *Set hiddenSearch context to determine if the searchBar will be displayed or not in Header; *Set avatar as faker image avatar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,99 @@
+// import Footer from "./components/Footer";
+// import { LABELS } from "./constants";
+// import HomePage from "./pages/HomePage";
+
+// function App() {
+//   return (
+//     <>
+//       <HomePage />
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import { faker } from "@faker-js/faker";
+import Footer from "./components/Footer";
+import LABELS from "./constants";
+import routes from "./routes/routes";
+import { RouteProps } from "./types/PropsData";
+import Header from "./components/Header";
+import useHiddenSearch from "./hooks/contexts/useHiddenSearch";
+// import {
+//   HiddenSearchProvider,
+//   useHiddenSearch,
+// } from "./contexts/HiddenSearchContext";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route exact path="/" component={HomePage} />
+//         {/* <Route path="/another" component={AnotherPage} /> */}
+//       </Switch>
+//       <Footer
+//         key="footer"
+//         label={LABELS.FOOTER}
+//         version={LABELS.VERSION}
+//         showCopyRight
+//       />
+//     <Router/>
+//   );
+// }
+// function App() {
+//   return (
+//     <Router>
+//       <Switch>
+//         {routes.map((route: RouteProps) => {
+//           return (
+//             <Route
+//               key={route.path}
+//               path={route.path}
+//               Component={route.Component}
+//             />
+//           );
+//         })}
+//         {/* <Route path="/another" component={AnotherPage} /> */}
+//       </Switch>
+//       <Footer
+//         key="footer"
+//         label={LABELS.FOOTER}
+//         version={LABELS.VERSION}
+//         showCopyRight
+//       />
+//     </Router>
+//   );
+// }
 function App() {
-  return <div>teste</div>;
+  const { hiddenSearch } = useHiddenSearch();
+  const avatar = faker.image.avatar();
+  return (
+    <Router>
+      {/* <HiddenSearchProvider> */}
+      <Header
+        srcAvatar={avatar}
+        altAvatar={LABELS.ALT_AVATAR}
+        type="text"
+        placeholder={`${LABELS.SEARCH}...`}
+        hiddenSearch={hiddenSearch}
+      />
+      <Routes>
+        {routes.map((route: RouteProps) => {
+          return (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={React.createElement(route.Component)}
+              // handle={}
+            />
+          );
+        })}
+      </Routes>
+      <Footer
+        key="footer"
+        label={LABELS.FOOTER}
+        version={LABELS.VERSION}
+        showCopyRight
+      />
+      {/* </HiddenSearchProvider> */}
+    </Router>
+  );
 }
 
 export default App;
