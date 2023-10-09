bd4fc591292eac6fde7c1a9001ef35d64191314e
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-10-08 21:52:06 -0300

    revert: :fire: [COMPONENT]Table
    *TitleAndCollapse, styles, Select, Root and index removed.

--- src/components/Table/Select.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { SelectProps } from 'components/Select/types';
-
-import { CustomizedSelect } from './styles';
-
-export default function TableSelect({ ...rest }: SelectProps) {
-  return <CustomizedSelect {...rest} />;
-}
