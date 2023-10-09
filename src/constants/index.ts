596992c3648914d35fda599be5106f4175546363
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-10-09 17:16:59 -0300

    feat: :art: [CONSTANT]constants
    *Created RoutesData; *Removed export default; *Added LINKEDIN AND MY_LAST_CHALLENGE to Labels.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,15 @@
-import { LabelsData } from "../types/ConstantsData";
+import { LabelsData, RoutesData } from "../types/ConstantsData";
 
-const LABELS: LabelsData = {
+export const LABELS: LabelsData = {
   FOOTER: "Acacio 2021",
   VERSION: "0.1.0",
-  SEARCH: "Search",
+  SEARCH: "Search an animal...",
   ALT_AVATAR: "Avatar",
+  LINKEDIN: "LinkedIn",
+  MY_LAST_CHALLENGE: "My last challenge",
 };
 
-export default LABELS;
+export const ROUTES: RoutesData = {
+  MY_LINKEDIN: "https://www.linkedin.com/in/acacio-lacerda/",
+  MY_LAST_CHALLENGE: "https://github.com/LacerdaAcacio/currency-exchange",
+};
