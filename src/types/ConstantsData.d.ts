3bd8ab062e5f06564528dab2af1032c7b82a61d8
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-10-09 21:53:02 -0300

    feat: :label: [TYPE]ConstantsData
    *Added NO_RESULTS, TRY_LOOKING, and ANIMAL_OPTIONS to LabelsData.

--- src/types/ConstantsData.d.ts
+++ src/types/ConstantsData.d.ts
@@ -5,6 +5,9 @@ export interface LabelsData {
   ALT_AVATAR: string;
   LINKEDIN: string;
   MY_LAST_CHALLENGE: string;
+  NO_RESULTS: string;
+  TRY_LOOKING: string;
+  ANIMAL_OPTIONS: string;
 }
 
 export interface RoutesData {
